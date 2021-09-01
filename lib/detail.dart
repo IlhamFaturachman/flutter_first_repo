ba9df6881f2ef5a9fd8e95c2eb37a9d96ac9d473
IlhamFaturachman <69384419+IlhamFaturachman@users.noreply.github.com>
2021-09-01 12:32:16 +0700

    Mir4

--- lib/detail.dart
+++ lib/detail.dart
@@ -9,12 +9,94 @@ class Detail extends StatefulWidget {
 
 class _DetailState extends State<Detail> {
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xff161616),
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xff161616),
+      ),
+      drawer: NavigationDrawerWidget(),
+      body: SingleChildScrollView(
+        child: Container(
+          color: const Color(0xff161616),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 30, 0, 30),
+                child: Container(
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Recommended Movies",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 30, 0, 20),
+                child: Container(
+                  width: size.width * 0.9,
+                  height: size.height * 0.3,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/heri.jpg"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 0, 30),
+                child: Container(
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Harry Potter",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 0, 30),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      child: Text(
+                        "Lorem dolor sit amet, consectetur adipiscing elit. Maecenas vitae elementum lectus, quis porta quam. Morbi cursus iaculis velit in eleifend. Nam rutrum erat in lectus laoreet ultrices. Ut aliquet, purus at",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                            decoration: TextDecoration.none),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      child: Text(
+                        "Lorem dolor sit amet, consectetur adipiscing elit. Maecenas vitae elementum lectus, quis porta quam. Morbi cursus iaculis velit in eleifend. Nam rutrum erat in lectus laoreet ultrices. Ut aliquet, purus at",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                            decoration: TextDecoration.none),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-        drawer: NavigationDrawerWidget(),
-        body: SingleChildScrollView(
-        ),
-      );
+      ),
+    );
+  }
 }
